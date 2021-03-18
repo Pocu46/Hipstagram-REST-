75ba8c8fce123c0e377675ae8f41747db26d30aa
pocu46 <andrew.pulyaev@gmail.com>
2021-03-18 16:17:40 +0200

    life cycle methods, componentDidMount added

--- hipstagram/src/Content/Users/users.js
+++ hipstagram/src/Content/Users/users.js
@@ -3,10 +3,7 @@ import "./users.css";
 import * as axios from 'axios';
 
 class Users extends React.Component {
-
-    constructor(props) {
-        super(props);
-
+    componentDidMount() {
         axios.get("https://social-network.samuraijs.com/api/1.0/users")
         .then(response => {
             this.props.setUsers(response.data.items);
@@ -45,48 +42,5 @@ class Users extends React.Component {
         )
     }
 }
-
-// let Users = (props) => {
-
-//     let getUsers = () => {
-//         if (props.users.length === 0) {
-//             axios.get("https://social-network.samuraijs.com/api/1.0/users")
-//                 .then(response => {
-//                     props.setUsers(response.data.items);
-//                 });
-//         }
-//     }
-
-//     return (
-//         <div className='users-wrapper'>
-//             <button onClick={getUsers}>Get Users</button>
-//             {
-//                 props.users.map(u => <div key={u.id}>
-//                     <span>
-//                         <div>
-//                             <img src={u.photos.small !=null ? u.photos.small : "21104.png"} alt='nothing' className='userPhoto' />
-//                         </div>
-//                         <div>
-//                             {u.followed
-//                                 ? <button onClick={() => { props.unfollow(u.id) }}>Unfollow</button>
-//                                 : <button onClick={() => { props.follow(u.id) }}>Follow</button>}
-//                         </div>
-//                     </span>
-
-//                     <span>
-//                         <span>
-//                             <div>{u.name}</div>
-//                             <div>{u.status}</div>
-//                         </span>
-//                         <span>
-//                             <div>{"u.location.country"}</div>
-//                             <div>{"u.location.city"}</div>
-//                         </span>
-//                     </span>
-//                 </div>)
-//             }
-//         </div>
-//     )
-// }
 
 export default Users;
