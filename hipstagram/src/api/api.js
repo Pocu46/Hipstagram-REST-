8b719a33372443b868fce4b55919a31615cbfe49
pocu46 <andrew.pulyaev@gmail.com>
2021-03-29 21:11:47 +0300

    axios.create added

---
+++ hipstagram/src/api/api.js
@@ -0,0 +1,23 @@
+import * as axios from 'axios';
+
+const instance = axios.create({
+    withCredentials: true,
+    baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+    headers: {
+        "API-KEY": "2d78aed0-6ada-4ee0-9316-708ff47c7f0d"
+    }
+});
+
+export const usersAPI = {
+    getUsers(currentPage = 1, pageSize = 10) {
+        return instance.get(`users?page=${currentPage}&count=${pageSize}`).then(response => response.data);
+    },
+}
+
+// export const getUsers = (currentPage = 1, pageSize = 10) => {
+//     return instance.get(`users?page=${currentPage}&count=${pageSize}`).then(response => response.data);
+// }
+
+export const getUsers2 = (currentPage = 1, pageSize = 10) => {
+    return axios.get(`follow?page=${currentPage}&count=${pageSize}`).then(response => response.data);
+}
