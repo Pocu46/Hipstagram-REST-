0cf94dbc80051a6efd129334cf2442af2f23b238
pocu46 <andrew.pulyaev@gmail.com>
2021-04-15 23:00:59 +0300

    local state added

--- hipstagram/src/Content/Profile/ProfileInfo/profileInfo.js
+++ hipstagram/src/Content/Profile/ProfileInfo/profileInfo.js
@@ -1,21 +1,22 @@
 import Preloader from '../../Common/Preloader/Preloader';
 import './profileInfo.css';
+import ProfileStatus from './ProfileStatus';
 
 const ProfileInfo = (props) => {
-    if(!props.profile) {
+    if (!props.profile) {
         return <Preloader />
     }
 
     return (
         <div>
-            <div>
+            {/* <div>
                 <img src="https://images.ctfassets.net/hrltx12pl8hq/6bi6wKIM5DDM5U1PtGVFcP/1c7fce6de33bb6575548a646ff9b03aa/nature-photography-pictures.jpg?fit=fill&w=800&h=300"></img>
-            </div>
+            </div> */}
 
             <div className="profile-avatar">
                 <img src={props.profile.photos.large} alt='no image' />
-        ava + description
-      </div>
+                <ProfileStatus status={"Hello you guys"} />
+            </div>
         </div>
     );
 };
